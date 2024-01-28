ff2dd24d8655afa01eecb210e6120f28a056a616
Bavigadda Prudhviraj <prudhviraj729@gmail.com>
2024-01-28 23:40:42 +0530

    Pending

---
+++ POD_2024/src/com/prudhvi/geeks_for_geeks/january/Jan_28_GeekinaHate1s.java
@@ -0,0 +1,10 @@
+package com.prudhvi.geeks_for_geeks.january;
+
+public class Jan_28_GeekinaHate1s {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ POD_2024/src/com/prudhvi/leetcode/january/Jan_28_1074_NumberOfSubmatricesThatSumToTarget.java
@@ -0,0 +1,10 @@
+package com.prudhvi.leetcode.january;
+
+public class Jan_28_1074_NumberOfSubmatricesThatSumToTarget {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
